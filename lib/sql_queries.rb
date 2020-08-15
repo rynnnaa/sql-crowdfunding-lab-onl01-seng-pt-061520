fad43bc4a7b9bdfb9f7f69a46c6d14cb98beac1b
Ryna Truong <rynatruong@outlook.com>
2020-08-15 22:55:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount
-  FROM users
+  FROM user
   JOIN pledges
   WHERE user.id = pledges.user_id
   GROUP BY users.name
