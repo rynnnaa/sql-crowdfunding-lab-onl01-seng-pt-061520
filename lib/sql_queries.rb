ce0ab81aff71ec0b11accda85a83ab6db991602f
Ryna Truong <rynatruong@outlook.com>
2020-08-15 23:06:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,5 +52,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
   JOIN pledges
-  ON 
+  ON projects.id = pledges.project_id
+  WHERE category = 'books'"
 end
