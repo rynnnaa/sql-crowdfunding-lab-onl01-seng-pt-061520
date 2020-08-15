72a1d40fdd51a566d9f3385b241abb898789201b
Ryna Truong <rynatruong@outlook.com>
2020-08-15 22:57:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, SUM(pledges.amount)
   FROM pledges
   JOIN projects
-  WHERE pledges.project_id = project.id
+  WHERE pledges.project_id = projects.id
   GROUP BY projects.category"
 end
 
