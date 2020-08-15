bab462cc55f5fe0ae7ea131ebf7a455703ca9dae
Ryna Truong <rynatruong@outlook.com>
2020-08-15 23:04:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,7 +45,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY projects.category
   WHERE category = 'music'"
 end
 
